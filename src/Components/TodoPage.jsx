1c604823dcac306dab2caf239a5b75c592362135
BaHGeT <abosama150@gmail.com>
2022-09-29 13:53:33 +0200

    edit remove button

--- src/Components/TodoPage.jsx
+++ src/Components/TodoPage.jsx
@@ -21,8 +21,22 @@ const TodoPage =({userName, todos, setTodos}) =>{
 
     const handleremove = (e) =>{
         e.preventDefault();
-        const item = e.target.parentNode.children[0].textContent;
-        setTodos(todos.filter(obj => obj.todo !==item))
+        console.log(e)
+        if(e.target.localName === 'button'){
+            const item = e.target.parentNode.children[0].textContent;
+            setTodos(todos.filter(obj => obj.todo !==item))
+            console.log(e.target.localName)
+        } 
+        if(e.target.localName === 'path'){
+            const item = e.target.parentNode.parentNode.parentNode.children[0].textContent;
+            setTodos(todos.filter(obj => obj.todo !==item))
+            console.log(e.target.localName)
+        }
+        if(e.target.localName === 'svg'){
+            const item = e.target.parentNode.parentNode.children[0].textContent;
+            setTodos(todos.filter(obj => obj.todo !==item))
+            console.log(e.target.localName)
+        }
     }
 
     return (
@@ -40,7 +54,11 @@ const TodoPage =({userName, todos, setTodos}) =>{
                     return (
                     <li key={i} className='todo-list-item'>
                         <h2>{todo.todo}</h2>
-                        <button onClick={handleremove} className='todo-list-item-button'></button>
+                        <button onClick={handleremove} className='todo-list-item-button'>
+                            <svg onClick={handleremove} class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon">
+                                <path onClick={handleremove} d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
+                            </svg>
+                        </button>
                     </li>
                     
                     );
