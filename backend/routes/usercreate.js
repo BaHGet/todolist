4fcc89634c2e8d24b6e693a5ad40719a35add656
BaHGet <abosama150@gmail.com>
2024-06-28 16:16:58 +0300

    upload api to netlify

--- backend/routes/usercreate.js
+++ backend/routes/usercreate.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../models/user");
-import queryString from 'query-string';
 const { Hashing, compareing } = require("../utilities/HashingAlgo");
 
 /* 
