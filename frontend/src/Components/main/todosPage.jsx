155624a4ef2c55aa3b2590e24f14504ec04e15ce
BaHGet <abosama150@gmail.com>
2024-07-03 22:57:20 +0300

    add todo page component

--- frontend/src/Components/main/todosPage.jsx
+++ frontend/src/Components/main/todosPage.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react"
+import { getTodos } from "../../todosApi"
+import Todo from "./todo"
+import NavDropdown from "./navDropdown"
 
-const TodosPage = () => {
+
+const TodosPage = ({user, handelLogout}) => {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        getTodos(user.username).then(res => {
+            setTodos(res)
+        })
+    }, [])
+    
     return (
-        <div>
-            <h1>todos</h1>
-        </div>
+        <>
+            <div className="d-flex justify-content-center align-items-center p-2" style={{backgroundColor: '#a7adba'}}>
+                <h2 className="m-3">Welcome {user.username}</h2>
+                <NavDropdown user={user} handelLogout={handelLogout}/>
+            </div>
+            {
+                todos.map(todo => <Todo key={todo.id} todo={todo}/>)
+            }
+        </>
     )
 }
 
