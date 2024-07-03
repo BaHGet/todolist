bc21bd3b9702bb89a7b616d93b0b69be5fb4168b
BaHGet <abosama150@gmail.com>
2024-07-04 00:28:11 +0300

    finish design todos gallary

--- frontend/src/Components/main/todo.jsx
+++ frontend/src/Components/main/todo.jsx
@@ -1,14 +1,77 @@
+import { useState } from 'react';
+import { Button, Card, ListGroup, Nav, Form} from 'react-bootstrap';
 import dayjs from 'dayjs'
 import relativeTime from "dayjs/plugin/relativeTime";
+import { deleteTodo, updateTodo } from '../../APIs/todosApi';
 
 dayjs.extend(relativeTime);
 
-const Todo = ({todo}) => {
+const Todo = ({index, todo}) => {
+const [edit , setEdit] = useState(false)
+const [editTitle, setEditTitle] = useState(todo.title)
+const [editDescription, setEditDescription] = useState(todo.description)
+const [editPraiority, setEditPraiority] = useState(todo.status) 
+
+const handelEdit = (bool) => {
+    setEdit(bool)
+    setEditTitle(todo.title)
+    setEditDescription(todo.description)
+    setEditPraiority(todo.status)
+}
+
+const handelUpdate = async() => {
+    handelEdit(false)
+    await updateTodo(todo.username, todo.title,editTitle, editDescription, editPraiority)
+    setEditTitle('')
+    setEditDescription('')
+    setEditPraiority('')
+}
+const handelDelete = () => {
+    deleteTodo(todo.username, todo.title)
+}
+
     return (
-        <div className="d-flex justify-content-between align-items-center mb-3">
-            <h3 className="m-1">{todo.title}</h3>
-            <p className="m-1">{todo.description}</p>
-            <p className="m-1">{dayjs(todo.created_at).fromNow()}</p>
+        <div key={index}>
+                <Card key={'card-'+index} style={{ width: '20rem', margin: '5px', backgroundColor: '#c0c5ce', color: '#4f5b66'}}>
+                <Card.Header style={{backgroundColor: '#a7adba'}}>
+                    <Nav variant="tabs" defaultActiveKey="#todo">
+                        <Nav.Item>
+                            <Nav.Link href="#todo" className='text-dark' onClick={() => handelEdit(false)}>Active</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#edit" className='text-dark' onClick={() =>handelEdit(true)}>Edit</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Card.Header>
+                    <Card.Body key={'card-body-' + index}>
+                        {
+                            edit ?
+                                    <Form.Control size="sm" className='my-2' type="text" placeholder={todo.title} onChange={(e) => setEditTitle(e.target.value)}  /> 
+                                :
+                                    <Card.Title key={'card-title-' + index}>{todo.title}</Card.Title>
+                        }
+                        <Card.Subtitle key={'card-subtitle-' + index} className="fw-lighter">Created {dayjs(todo.created_at).fromNow()}</Card.Subtitle>
+                    </Card.Body>
+                    <ListGroup key={'list-' + index} className="list-group-flush">
+                        {
+                            edit ?
+                                    <>
+                                        <Form.Control size="sm" className='' type="text" placeholder="new description" onChange={(e) => setEditDescription(e.target.value)}  /> 
+                                        <Form.Control size="sm" className='my-1' type="text" placeholder='praiority' onChange={(e) => setEditPraiority(e.target.value)}  /> 
+                                    </>
+                                :
+                                    <>
+                                        <ListGroup.Item key={'description-'+index} className='text-break'>{todo.description}</ListGroup.Item>
+                                        <ListGroup.Item key={'priority-'+index}>Priority: </ListGroup.Item>
+                                    </>
+                        }
+                        
+                        <ListGroup.Item key={'buttons-'+index} className='d-flex justify-content-around align-items-center'>
+                            <Button key={'done-'+index} variant={edit ? 'dark' : 'mute'} disabled={!edit} onClick={handelUpdate} size="sm">Done</Button>
+                            <Button key={'delete-'+index} variant="danger" onClick={handelDelete} size="sm">Delete</Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card>
         </div>
     )
 }
--- frontend/src/Components/main/todosPage.jsx
+++ frontend/src/Components/main/todosPage.jsx
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react"
 import { getTodos } from "../../APIs/todosApi"
+import "./todo-style.css"
 import Todo from "./todo"
 import NavDropdown from "./navDropdown"
 
 
 const TodosPage = ({user, handelLogout}) => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+    const [width, setWidth] = useState(window.outerWidth);
+
+    useEffect(() => {
+        const handleWindowResize = () => setWidth(window.outerWidth);
+        window.addEventListener("resize", handleWindowResize);
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, [width]);
 
     useEffect(() => {
         getTodos(user.username).then(res => {
@@ -15,13 +25,20 @@ const TodosPage = ({user, handelLogout}) => {
 
     return (
         <>
-            <div className="d-flex justify-content-center align-items-center p-2" style={{backgroundColor: '#a7adba'}}>
+            <div className="d-flex justify-content-center align-items-center" style={{backgroundColor: '#a7adba'}}>
                 <h2 className="m-3">Welcome {user.username}</h2>
                 <NavDropdown user={user} handelLogout={handelLogout}/>
             </div>
-            {
-                todos.map(todo => <Todo key={todo.id} todo={todo}/>)
-            }
+            <div id="todos-container" className={
+                    width < 768 ? 
+                        "d-flex flex-column justify-content-center align-items-center p-2" 
+                    :
+                        "d-flex flex-row flex-wrap justify-content-center align-items-center p-2 "
+                }>
+                {
+                    todos.map(todo => <Todo index={todo._id} todo={todo}/>)
+                }
+            </div>
         </>
     )
 }
