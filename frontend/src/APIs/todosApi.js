918b070825f721dc6b36d9111fc302b985d8036e
BaHGet <abosama150@gmail.com>
2024-07-04 00:27:15 +0300

    adds updateTodo, deleteTodo methods

--- frontend/src/APIs/todosApi.js
+++ frontend/src/APIs/todosApi.js
@@ -33,3 +33,38 @@ export const createTodo = async (data) => {
     const res = await response.json()
     return res
 }
+
+export const updateTodo = async (username, title, newTitle, newdDescription, newPriority) => {
+    let santData = {
+        username: username,
+        title: title,
+        newTitle: newTitle,
+        newdDescription: newdDescription,
+        newPriority: newPriority
+    }
+    const response = await fetch(url,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'cors': 'no-cors'
+            },
+            body: JSON.stringify(santData)
+        })  
+    const res = await response.json()
+    return res
+}
+
+export const deleteTodo = async (username, title) => {
+    const response = await fetch(url,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'cors': 'no-cors'
+            },
+            body: JSON.stringify({username, title})
+        })  
+    const res = await response.json()
+    return res 
+}
