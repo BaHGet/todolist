f82b8cc1be301f1dc2593979bbd39011a8253dc7
BaHGet <abosama150@gmail.com>
2024-07-05 18:47:59 +0300

    a component to create the add todo page

---
+++ frontend/src/Components/main/addTodo.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Form, Button, FloatingLabel } from "react-bootstrap"
+import { createTodo } from "../../APIs/todosApi";
+
+const AddTodo = ({username}) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority , setPriority] = useState('');
+    const [dueDate , setDueDate] = useState('');
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+        createTodo({username,title, description, priority, dueDate}).then(res => { console.log(res)})
+        window.location.reload();
+    };
+    return (
+        <Form 
+            className="d-flex flex-column justify-content-center align-items-center p-2 m-2 rounded" 
+            style={{backgroundColor: '#a7adba'}}
+            validated={validated}
+        >
+            <FloatingLabel controlId="floatingInputName" label="Add Title" className="mb-3 w-100 text-dark">
+                <Form.Control required type="name" placeholder="Add Title" onChange={(e) => setTitle(e.target.value)} />
+                <Form.Control.Feedback type="invalid">
+                    Please type a title.
+                </Form.Control.Feedback>
+        
+            </FloatingLabel>
+            <Form.Group controlId="floatingInputName" label="Add Description" className="mb-3 w-100 text-dark">
+                <Form.Control required as="textarea" type="text" placeholder="Add Description" onChange={(e) => setDescription(e.target.value)} />    
+                <Form.Control.Feedback type="invalid">
+                    Please type a Description.
+                </Form.Control.Feedback>
+        
+            </Form.Group>
+            <FloatingLabel controlId="floatingInputName" label="Add Priority" className="mb-3 w-100 text-dark">
+                <Form.Control required as="input" type="text" placeholder="Add Priority" onChange={(e) => setPriority(e.target.value)}/>
+                <Form.Control.Feedback type="invalid">
+                    Please type a Priority.
+                </Form.Control.Feedback>
+            </FloatingLabel>
+            <Form.Group label="Add Due Date" className="mb-3 w-100 text-dark">
+                <Form.Label>Due Date:  </Form.Label>
+                <input required type="datetime-local"  onChange={(e) => setDueDate(e.target.value)}/>
+                <Form.Control.Feedback type="invalid">
+                    Please type a Due Date.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Button variant="dark" type="submit" onClick={(e) => { handleSubmit(e) }}>Add</Button>
+        </Form>
+    )
+}
+
+export default AddTodo
