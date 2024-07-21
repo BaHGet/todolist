43d01dc8a725a0883af711e6b3962f70752cfa3d
BaHGet <abosama150@gmail.com>
2024-07-22 01:13:34 +0300

    install serverless-http library

--- model/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const taskschema = new mongoose.Schema({
-    name: {type: String, required: true},
-    describetion: {type: String, required: false},
-    completed: {type: Boolean, default: false}
-})
-
-module.exports = mongoose.model("Task", taskschema)
