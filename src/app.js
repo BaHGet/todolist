f15c7e0386c97b8dfa61a26d553260057c595977
BaHGet <abosama150@gmail.com>
2024-07-22 01:13:06 +0300

    add serverless handleing & remove unnecessary comment @line:07

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
-// requrie function
 const express = require("express");
+const serverless = require("serverless-http");
 const mongoose = require("mongoose");
-const taskRoute = require("../routes/TaskRoutes")
-require('dotenv').config();
+require("dotenv").config();
 const dbPassword = process.env.DB_PASSWORD;
 const app = express();
 
-
-// MiddleWare
-app.use(express.json())
-
-
+app.use(express.json());
 
 // mongoose conection
 mongoose.connect(
-    `mongodb+srv://mahad10taha:${dbPassword}@cluster0.jmtourk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/todo_list`
-)
+  `mongodb+srv://mahad10taha:${dbPassword}@cluster0.jmtourk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/todo_list`
+);
 const db = mongoose.connection;
-db.on("error", ()=> {
-    console.log('error connection')
-})
-db.once("open", ()=> {
-    console.log("connection succussfuly")
-})
+db.on("error", () => {
+  console.log("error connection");
+});
+db.once("open", () => {
+  console.log("connection succussfuly");
+});
 
-
-app.use(taskRoute);
+const todoRoute = require("../routes/todoRoutes");
+app.use(todoRoute);
 
 // liston port
 const port = 4444;
-app.listen(port, ()=> console.log(`start server on port ${port}`));
+app.listen(port, () => console.log(`start server on port ${port}`));
+
+module.exports.handler = serverless(app);
